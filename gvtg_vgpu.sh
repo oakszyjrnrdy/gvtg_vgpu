1b8923d984abc95bd7eaabb347859162fb9cc3f2
oakszyjrnrdy <26323021+oakszyjrnrdy@users.noreply.github.com>
2020-08-05 19:14:32 +0800

    feat: add systemd files

---
+++ gvtg_vgpu.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+
+# Workaround to deal with the problem that the system cannot suspend if gvtg_vgpu is on
+# Remove gvtg_vgpu before suspend or hibernation
+# Recreate gvtg_vgpu after waking from suspend or hibernation
+
+case $1 in
+    pre)
+	/usr/bin/systemctl stop gvtg_vgpu.service
+	;;
+    post)
+	/usr/bin/systemctl start gvtg_vgpu.service
+	;;
+esac
